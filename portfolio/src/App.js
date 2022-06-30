7597aadaffc855ba99137827423fd26cd96c41b9
nathangreiling <ngreiling20@gmail.com>
2022-06-29 23:15:26 -0500

    About section
    start on about section
    needs styling, import selfie image, continuation of bio

---
+++ portfolio/src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import About from './components/About'
+
+function App() {
+  return (
+    <div>
+      <main>
+        <About></About>
+      </main>
+    </div>
+  );
+}
+
+export default App;
---
+++ portfolio/src/components/About/index.js
@@ -0,0 +1,16 @@
+import React from 'react';
+// import selfie from '../../assets/images/'
+
+
+function About() {
+  return (
+    <section className="">
+      <h1 id="about">About Me</h1>
+      {/* <img src={selfie} className='' style={{ width: "35%" }} alt="selfie"></img> */}
+      <h2>Aspiring Web-Developer with a passion to create, resilience to overcome challenges, and the flexibility to work with others.</h2>
+      <div>Creation is the start of all great things. Over my career in software engineering, I have been developing my skills in order to create applications...</div>
+    </section>
+  );
+}
+
+export default About;
